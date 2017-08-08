731fbad692ee065b211cf87b8418b19a00470421
ciangottinim <manuel.ciangottini@gmail.com>
2017-08-08 12:36:51 +0200

    added oneclient mount

--- run.sh
+++ run.sh
@@ -63,6 +63,7 @@ while getopts ':m:e:s:c:k:u:p:C:P:S:I:' OPTION; do
       echo "CCB_ADDRESS = $OPTARG" >> /etc/condor/condor_config
       mkdir -p /mnt/onedata/
       mkdir -p /var/log/dodas
+      oneclient -i -o allow_other -H $CMS_ONEDATA_CACHE -t $ONEDATA_ACCESS_TOKEN /mnt/onedata/
       HEALTH_CHECK='executor'
     ;;
     c)
